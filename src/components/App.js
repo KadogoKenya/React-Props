ef581b9129b777c3e5ea266ab161a38e6aa5ec12
KadogoKenya <catherinekanini001@gmail.com>
2025-07-21 23:00:49 +0300

    added tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import NavBar from "./NavBar";
+import user from "../data/user";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
